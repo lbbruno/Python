2942a994ed1eda159c5564529f4e8ad8785c1c86
lbbruno <73141520+lbbruno@users.noreply.github.com>
2021-02-28 18:14:09 -0300

    Update 010__Formatando_Strings_4.py

--- Exercicios/010__Formatando_Strings_4.py
+++ Exercicios/010__Formatando_Strings_4.py
@@ -1,5 +1,4 @@
 # Formatando Strings
-````
+#___________________________________________________________________________
 c = float(input('Quanto dinheiro você tem na carteira? '))
 print('Com R${:.2f} Você pode comprar {:.2f} dolares'.format(c, c/3.27))
-```
